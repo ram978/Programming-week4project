2a64af50a5563d175f39db461819ca9efbe71747
Janakiram <janakiram_sundaraneedi@student.uml.edu>
2016-07-26 03:55:10 -0400

    solution to ranl hospital.R

---
+++ rankhospital.R
@@ -0,0 +1,37 @@
+rankhospital <- function(state, outcome, num = "best"){
+  ## Read outcome data
+  outcome_data <- read.csv("outcome-of-care-measures.csv", colClasses = "character", header =TRUE, na.strings = "Not Available")
+  outcome_need <- outcome_data[, c("Hospital.Name", "State", "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia", "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure", "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack")]
+  cnames <-c("Hospital.Name", "State", "Pneumonia", "Heart.Failure", "Heart.Attack")
+  colnames(outcome_need)<-cnames
+  names(outcome_need)
+  ## Check that state and outcome are valid
+  if(!(state %in% outcome_need$State)){
+    stop("invalid state")
+  }
+  
+  if (! outcome %in% c("heart attack","heart failure","pneumonia")){
+    stop("invalid outcome")
+  } 
+  if(outcome == "heart attack") {
+    x <-5
+  } else if(outcome == "heart failure") {
+    x <-4
+  } else if(outcome == "pneumonia") {
+    x <-3
+  }#outcome_heart_attack <- outcome_need[, c("Hospital.Name", "State", "Heart.Failure")]
+  state_set=subset(outcome_need, outcome_need$State == state)
+  View(state_set)
+  state_set[,x] <- as.numeric(state_set[,x])
+  orderdata <- state_set[order(state_set[, x], state_set[, 1], na.last = NA), ]
+  #min_of_state <- min(state_set[,x], na.rm = TRUE)
+  #result_set <- subset(state_set,state_set[,x]  == min_of_state)
+  #result_set$Hospital.Name
+  if(num == "best") {
+    orderdata[1, 2]
+  } else if(num == "worst") {
+    orderdata[nrow(orderdata), 1]
+  } else{
+    orderdata[num, 1]
+  }
+}
