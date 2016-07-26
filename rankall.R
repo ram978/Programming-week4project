b80f0e36fc3826fabbcd7d1ce06a6488adecdfe2
Janakiram <janakiram_sundaraneedi@student.uml.edu>
2016-07-26 03:54:05 -0400

    Rankall solution

---
+++ rankall.R
@@ -0,0 +1,22 @@
+rankall <- function(outcome, num="best"){
+  ## Read outcome data
+  outcome_data <- read.csv("outcome-of-care-measures.csv", colClasses = "character", header =TRUE, na.strings = "Not Available")
+  outcome_need <- outcome_data[, c("Hospital.Name", "State", "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia", "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure", "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack")]
+  cnames <-c("Hospital.Name", "State", "Pneumonia", "Heart.Failure", "Heart.Attack")
+  colnames(outcome_need)<-cnames
+  names(outcome_need)
+  ## Check that state and outcome are valid
+  if(!(state %in% outcome_need$State)){
+    stop("invalid state")
+  }
+  
+  if (! outcome %in% c("heart attack","heart failure","pneumonia")){
+    stop("invalid outcome")
+  } 
+  if(outcome == "heart attack") {
+    x <-5
+  } else if(outcome == "heart failure") {
+    x <-4
+  } else if(outcome == "pneumonia") {
+    x <-3
+  }
